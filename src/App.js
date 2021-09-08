8a90724b2a911c5b57e61000e5c0d9955f37c648
Georjane <pzgeorjane@gmail.com>
2021-09-08 17:41:18 +0100

    Add home and dashboard component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, BrowserRouter, Switch, } from 'react-router-dom'
+import Dashboard from './components/Dashboard';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={"/"} component={Home} />
+          <Route exact path={"/dashboard"} component={Dashboard} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
