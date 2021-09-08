dbe2f067e98bb8b45eca7f3ef56695184768f8b1
Georjane <pzgeorjane@gmail.com>
2021-09-08 17:50:06 +0100

    Add registration component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Registration from './auth/Registration';
 
 export default class Home extends Component {
   constructor(props) {
@@ -10,6 +11,7 @@ export default class Home extends Component {
       <div>
         <h1>Home</h1>
         <h1>Home</h1>
+        <Registration />
       </div>
     );
   }
