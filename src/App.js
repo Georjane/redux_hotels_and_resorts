361912e23e5c1b4fb1008dbad845f49e4a815676
Georjane <pzgeorjane@gmail.com>
2021-09-08 17:46:14 +0100

    add axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, BrowserRouter, Switch, } from 'react-router-dom'
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Home from './components/Home';
 
@@ -8,8 +8,8 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path={"/"} component={Home} />
-          <Route exact path={"/dashboard"} component={Dashboard} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/dashboard" component={Dashboard} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,10 @@
-import React, { Component } from "react";
+import React from 'react';
 
-const Dashboard = () => {
-  return (
-    <div>
-      <h1>Dashboard</h1>
-      <h1>Dashboard</h1>
-    </div>
-  )
-}
+const Dashboard = () => (
+  <div>
+    <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
+  </div>
+);
 
 export default Dashboard;
