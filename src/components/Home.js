3cbd3c99060737f56e650fd3911756705168d5e1
Georjane <pzgeorjane@gmail.com>
2021-09-08 18:11:03 +0100

    Add react form for sign up

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,25 @@ import Registration from './auth/Registration';
 export default class Home extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: '',
+      // email: '',
+      // password: '',
+      // password_confirmation: '',
+      // registrationErrors: '',
+    };
+
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
   }
 
   render() {
+    const { username } = this.state;
     return (
       <div>
         <h1>Home</h1>
         <h1>Home</h1>
+        <h1>{username}</h1>
         <Registration />
       </div>
     );
--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -3,12 +3,42 @@ import React, { Component } from 'react';
 export default class Registration extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+      // registrationErrors: '',
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    console.log('handle change', e);
+    console.log(this.state);
+  }
+
+  handleSubmit(e) {
+    console.log('form submitted');
+    console.log(this.state);
+    e.preventDefault();
   }
 
   render() {
+    const {
+      username, email, password, passwordConfirmation,
+    } = this.state;
     return (
       <div>
-        <h1>Registration</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="username" placeholder="Username" value={username} onChange={this.handleChange} required />
+          <input type="email" name="email" placeholder="Email" value={email} onChange={this.handleChange} required />
+          <input type="password" name="password" placeholder="Password" value={password} onChange={this.handleChange} required />
+          <input type="password" name="passwordConfirmation" placeholder="Password Confirmation" value={passwordConfirmation} onChange={this.handleChange} required />
+          <button type="submit">Register</button>
+        </form>
       </div>
     );
   }
