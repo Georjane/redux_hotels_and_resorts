3e8bdb78f61e44c66b108ef1eb7148efed13b582
Georjane <pzgeorjane@gmail.com>
2021-09-09 16:40:01 +0100

    Add navabar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Dashboard from './components/Dashboard';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 
-const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/dashboard" component={Dashboard} />
-    </Switch>
-  </BrowserRouter>
-);
+function App(props) {
+  const { storestate } = props;
 
-export default App;
+  return (
+    <BrowserRouter>
+      {storestate[0] === 'LOGGED_IN' ? <Navbar /> : <h1>PLEASE OUT</h1>}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+App.propTypes = {
+  storestate: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  storestate: state,
+});
+
+export default connect(mapStateToProps)(App);
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import Dashboard from './Dashboard';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <h1>Navbar</h1>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default Navbar;
