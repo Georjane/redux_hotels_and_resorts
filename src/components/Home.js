8dd2bf641e8e30df93ed4e9fb69546f1170e347f
Georjane <pzgeorjane@gmail.com>
2021-09-09 08:19:40 +0100

    Connect components to store

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+// import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import Registration from './auth/Registration';
 
-export default class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      // email: '',
-      // password: '',
-      // password_confirmation: '',
-      // registrationErrors: '',
-    };
-
-    // this.handleSubmit = this.handleSubmit.bind(this);
-    // this.handleChange = this.handleChange.bind(this);
-  }
-
-  render() {
-    const { username } = this.state;
-    return (
-      <div>
-        <h1>Home</h1>
-        <h1>Home</h1>
-        <h1>{username}</h1>
-        <Registration />
-      </div>
-    );
-  }
+function Home(props) {
+  const { loggedInStatus } = props;
+  console.log(props);
+  console.log(loggedInStatus);
+  return (
+    <div>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <Registration />
+    </div>
+  );
 }
+
+Home.propTypes = {
+  loggedInStatus: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  loggedInStatus: state,
+});
+
+export default connect(mapStateToProps)(Home);
