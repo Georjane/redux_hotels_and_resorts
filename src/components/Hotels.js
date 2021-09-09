5ee0bcf60e7e4fc2d88f0ce618f756e5bfd5e1e3
Georjane <pzgeorjane@gmail.com>
2021-09-09 22:12:33 +0100

    Able to display image url from backend

--- src/components/Hotels.js
+++ src/components/Hotels.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Hotels() {
+  const [images, setImages] = useState([]);
+  // const images = [];
   axios.get('http://localhost:3001/favorites', { withCredentials: true })
     .then((res) => {
       console.log('favourites ', res);
@@ -9,9 +12,32 @@ function Hotels() {
       console.log('favourites ', err);
     });
 
+  useEffect(() => {
+    axios.get('http://localhost:3001/hotels', { withCredentials: true })
+      .then((res) => {
+        console.log('hotels ', res);
+        console.log('hotels ', res.data);
+        const jane = [];
+        res.data.forEach((element) => {
+          jane.push(element.image_url);
+          console.log(element.image_url);
+        });
+        setImages(jane);
+      })
+      .catch((err) => {
+        console.log('hotels ', err);
+      });
+  }, []);
+
   return (
     <div className="Hotels">
+      <ol>
+        {images.map((img) => (
+          <li key={img}><img src={img} alt="Logo" key={img} /></li>
+        ))}
+      </ol>
       <h1>Hotels</h1>
+      {/* <img src="https://i.postimg.cc/DzKyqkK8/Img-Placeholder-3.png" alt="jane" /> */}
     </div>
   );
 }
