46f0a864eb2666e0d344b76808e7faf5dac1ede1
Georjane <pzgeorjane@gmail.com>
2021-09-10 14:24:03 +0100

    Add reducers for auth

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -5,77 +5,71 @@ import {
   LOGIN_FAIL,
   LOGOUT,
   SET_MESSAGE,
-} from "./types";
+} from './types';
 
-import AuthService from "../services/auth.service";
+import AuthService from '../services/auth.service';
 
-export const register = (username, email, password) => (dispatch) => {
-  return AuthService.register(username, email, password).then(
-    (response) => {
-      dispatch({
-        type: REGISTER_SUCCESS,
-      });
+export const register = (username, email, password) => (dispatch) => AuthService.register(username, email, password).then(
+  (response) => {
+    dispatch({
+      type: REGISTER_SUCCESS,
+    });
 
-      dispatch({
-        type: SET_MESSAGE,
-        payload: response.data.message,
-      });
+    dispatch({
+      type: SET_MESSAGE,
+      payload: response.data.message,
+    });
 
-      return Promise.resolve();
-    },
-    (error) => {
-      const message =
-        (error.response &&
-          error.response.data &&
-          error.response.data.message) ||
-        error.message ||
-        error.toString();
+    return Promise.resolve();
+  },
+  (error) => {
+    const message = (error.response
+          && error.response.data
+          && error.response.data.message)
+        || error.message
+        || error.toString();
 
-      dispatch({
-        type: REGISTER_FAIL,
-      });
+    dispatch({
+      type: REGISTER_FAIL,
+    });
 
-      dispatch({
-        type: SET_MESSAGE,
-        payload: message,
-      });
+    dispatch({
+      type: SET_MESSAGE,
+      payload: message,
+    });
 
-      return Promise.reject();
-    }
-  );
-};
+    return Promise.reject();
+  },
+);
 
-export const login = (username, password) => (dispatch) => {
-  return AuthService.login(username, password).then(
-    (data) => {
-      dispatch({
-        type: LOGIN_SUCCESS,
-        payload: { user: data },
-      });
+export const login = (username, password) => (dispatch) => AuthService.login(username, password).then(
+  (data) => {
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: { user: data },
+    });
 
-      return Promise.resolve();
-    },
-    (error) => {
-      const message =
-        (error.response &&
-          error.response.data &&
-          error.response.data.message) ||
-        error.message ||
-        error.toString();
+    return Promise.resolve();
+  },
+  (error) => {
+    const message = (error.response
+          && error.response.data
+          && error.response.data.message)
+        || error.message
+        || error.toString();
 
-      dispatch({
-        type: LOGIN_FAIL,
-      });
+    dispatch({
+      type: LOGIN_FAIL,
+    });
 
-      dispatch({
-        type: SET_MESSAGE,
-        payload: message,
-      });
+    dispatch({
+      type: SET_MESSAGE,
+      payload: message,
+    });
 
-      return Promise.reject();
-    }
-  );
-};
+    return Promise.reject();
+  },
+);
 
 export const logout = () => (dispatch) => {
   AuthService.logout();
