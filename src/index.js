7ff18c25e4452aa14e6383b72ad8aee7ae288c32
Georjane <pzgeorjane@gmail.com>
2021-09-10 13:54:07 +0100

    Add signup n signin w axios

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import './index.css';
+// import { Provider } from 'react-redux';
+// import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <Provider>
-    <App />
-  </Provider>,
+  // <Provider>
+  <App />,
+  // </Provider>,
   document.getElementById('root'),
 );
