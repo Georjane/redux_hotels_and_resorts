9ebc010c8d079fc0dde565f6eb575314aae22695
Georjane <pzgeorjane@gmail.com>
2021-09-12 18:03:01 +0100

    Add styles for hero section

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import Routes from './Routes';
 import store from './store';
+import './App.css';
 
 ReactDOM.render(
   <Provider store={store}>
