dd0a8d4194d543454a119a4cd2d39ac596d561ff
Georjane <pzgeorjane@gmail.com>
2021-10-08 06:45:57 +0100

    Add user services

---
+++ src/services/user.service.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import authHeader from './auth-header';
+
+// const API_URL = 'https://redux-authentication-api.herokuapp.com/';
+const API_URL = "http://localhost:3001/";
+
+class UserService {
+  getHotels() {
+    return axios.get(API_URL + 'hotels', { headers: authHeader() });
+  }
+
+  getHotelsDetails(id) {
+    return axios.get(API_URL + 'hotels/' + id, { headers: authHeader() });
+  }
+
+  getFavorites() {
+    return axios.get(API_URL + 'favorites', { headers: authHeader() });
+  }
+}
+
+export default new UserService();
