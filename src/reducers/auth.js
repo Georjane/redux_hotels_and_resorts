32ec484a51e30567401f2992c494270e9b79601a
Georjane <pzgeorjane@gmail.com>
2021-10-08 10:40:44 +0100

    add root reducer

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,15 +1,60 @@
-const initialState = {
-  users: [],
-  isLoggedIn: false,
-  loginErr: false,
-  logout: false,
-};
+import  {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+} from '../actions/types'
+
+const user = JSON.parse(localStorage.getItem("user"));
+
+const initialState = user
+  ? { isLoggedIn: true, user }
+  : { isLoggedIn: false, user: null };
+
+// const initialState = {
+//   users: [],
+//   isLoggedIn: false,
+//   loginErr: false,
+//   logout: false,
+// };
 
 const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
+  const { type, payload } = action;
+  
+  switch (type) {
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+        isLoggedIn: false,
+      };
+    case REGISTER_FAIL:
+      return {
+        ...state,
+        isLoggedIn: false,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isLoggedIn: true,
+        user: payload.user,
+      };
+    case LOGIN_FAIL:
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: null,
+      };
+    case LOGOUT:
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: null,
+      };
+
     case 'RESET_TOAST': {
       const newState = {
-        ...state, loginErr: false, isLoggedIn: false,
+        ...state, loginErr: false,
       };
       return newState;
     }
@@ -19,38 +64,39 @@ const rootReducer = (state = initialState, action) => {
       };
       return newState;
     }
-    case 'ISLOGGEDIN': {
-      if (sessionStorage.getItem('user_id')) {
-        console.log('nnnnnnnnnnnnnnnnnn');
-        const newState = {
-          ...state, isLoggedIn: true,
-        };
-        return newState;
-      }
-      return state;
-    }
+    // case 'ISLOGGEDIN': {
+    //   if (sessionStorage.getItem('user_id')) {
+    //     console.log('nnnnnnnnnnnnnnnnnn');
+    //     const newState = {
+    //       ...state, isLoggedIn: true,
+    //     };
+    //     return newState;
+    //   }
+    //   return state;
+    // }
     case 'SIGNUP': {
-      if (!action.err) {
-        const { user } = action.payload.user;
-        sessionStorage.setItem('user_id', action.payload.user.id);
-        const newState = {
-          ...state, isLoggedIn: true, loginErr: false, users: [user],
-        };
-        return newState;
-      }
-      const newState = {
-        ...state, isLoggedIn: true, loginErr: true,
-      };
-      return newState;
+      return state
+      // if (!action.err) {
+      //   const { user } = action.payload.user;
+      //   sessionStorage.setItem('user_id', action.payload.user.id);
+      //   const newState = {
+      //     ...state, isLoggedIn: true, loginErr: false, users: [user],
+      //   };
+      //   return newState;
+      // }
+      // const newState = {
+      //   ...state, isLoggedIn: true, loginErr: true,
+      // };
+      // return newState;
     }
     case 'LOGIN': {
       const { user } = action.payload;
 
       console.log(action.err);
-      if (action.err) {
-        const newState = { ...state, isLoggedIn: false, loginErr: true };
-        return newState;
-      }
+      // if (action.err) {
+      //   const newState = { ...state, isLoggedIn: false, loginErr: true };
+      //   return newState;
+      // }
       // if (action.payload !== undefined && action.payload.token) {
       //   sessionStorage.setItem('token', action.payload.token);
       //   sessionStorage.setItem('user_id', user.id);
@@ -65,22 +111,23 @@ const rootReducer = (state = initialState, action) => {
       //   const newState = { ...state, isLoggedIn: false, loginErr: true };
       //   return newState;
       // }
-      if (user) {
-        sessionStorage.setItem('user_id', user.id);
-      }
-      const newState = {
-        ...state, isLoggedIn: true, hasSignedUp: true, loginErr: false, users: [user],
-      };
-      return newState;
+      // if (user) {
+      //   sessionStorage.setItem('user_id', user.id);
+      // }
+      // const newState = {
+      //   ...state, isLoggedIn: true, hasSignedUp: true, loginErr: false, users: [user],
+      // };
+      // return newState;
+      return state;
     }
     case 'LOGOUT': {
-      if (action.payload.status === 200) {
-        sessionStorage.removeItem('user_id');
-        const newState = {
-          ...state, isLoggedIn: false, logout: true, users: [],
-        };
-        return newState;
-      }
+      // if (action.payload.status === 200) {
+      //   sessionStorage.removeItem('user_id');
+      //   const newState = {
+      //     ...state, isLoggedIn: false, logout: true, users: [],
+      //   };
+      //   return newState;
+      // }
       return state;
     }
     default:
